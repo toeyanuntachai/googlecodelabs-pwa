f92bfda21433cdd6859bfe15ca07122140c6dfb4
Glitch (googlecodelabs-your-first-pwapp-hq6wr935u) <none>
2020-01-26 06:12:25 +0000

    🐃☺️ Checkpoint ./public/scripts/install.js:10685033/4917 ./public/service-worker.js:10685033/2

--- public/scripts/install.js
+++ public/scripts/install.js
@@ -42,13 +42,22 @@ function saveBeforeInstallPromptEvent(evt) {
  */
 function installPWA(evt) {
   // CODELAB: Add code show install prompt & hide the install button.
-  // CODELAB: Log user response to prompt.
   deferredInstallPrompt.prompt();
+  // CODELAB: Log user response to prompt.
+  deferredInstallPrompt.userChoice.then(choice => {
+    if (choice.outcome === "accepted") {
+      console.log("User accepted the A2HS prompt", choice);
+    } else {
+      console.log("User dismissed the A2HS prompt", choice);
+    }
+    deferredInstallPrompt = null;
+  });
   // Hide the install button, it can't be called twice.
   evt.srcElement.setAttribute("hidden", true);
 }
 
 // CODELAB: Add event listener for appinstalled event
+window.addEventListener('appinstalled', logAppInstalled);
 
 /**
  * Event handler for appinstalled event.
@@ -58,4 +67,5 @@ function installPWA(evt) {
  */
 function logAppInstalled(evt) {
   // CODELAB: Add code to log the event
+  console.log('Weather App was installed.', evt);
 }
